d53fb404216043549cd8e0cc157356f3e85f8e58
Tylyps <jakubb.chmielewskii@gmail.com>
2023-08-01 00:42:55 +0200

    Update some changes, add loader and hrefs for links, fix some grafics

--- menu_script.js
+++ menu_script.js
@@ -30,11 +30,23 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
 // alternative to DOMContentLoaded
 document.onreadystatechange = function () {
+	console.log("document ready state changed ", document.readyState)
 	if (document.readyState == "interactive") {
+		console.log("DOcument ready state is interactive")
 			// Initialize your application or run some code.
 	}
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-	
+	console.log("document loaded")
+})
+
+window.addEventListener('load', function () {
+	const pageLoading = document.querySelector(".pageLoading");
+	pageLoading?.classList?.add("hidding");
+	setTimeout(() => {
+		pageLoading?.remove();
+	}, 1000);
+	console.log("Page is loaded!");
+  // alert("It's loaded!")
 })
