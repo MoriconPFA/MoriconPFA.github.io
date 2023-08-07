056abdaa0cf42837748a2b6b8f639295c5a00c0a
Tylyps <jakubb.chmielewskii@gmail.com>
2023-08-07 20:34:58 +0200

    Cleaning and small fixes

--- scripts/sponsors.js
+++ scripts/sponsors.js
@@ -3,16 +3,18 @@ const sponsorsContainer = document.querySelector("#sponsors .container");
 let animationIsRunning = false;
 let sponsorsClone = null;
 
+const animationBreakpoint = 1200;
+
 const onWindowResize = () => {
 	// console.log("ON WINDOW RESIZE!", window.innerWidth);
 	// console.log("ON WINDOW RESIZE!", sponsorsContainer, sponsorsContainer.offsetWidth);
 	// console.log(window.innerWidth, sponsorsContainer.offsetWidth, sponsorsContainer.getBoundingClientRect())
-	if (window.innerWidth < 1540 && !animationIsRunning) {
+	if (window.innerWidth < animationBreakpoint && !animationIsRunning) {
 		sponsorsContainer.classList.add("animation");
 		sponsorsClone = sponsorsContainer.cloneNode(true);
 		sponsorsSection.appendChild(sponsorsClone);
 		animationIsRunning = true;
-	} else if (window.innerWidth > 1540 && animationIsRunning) {
+	} else if (window.innerWidth > animationBreakpoint && animationIsRunning) {
 		sponsorsSection.removeChild(sponsorsClone);
 		animationIsRunning = false;
 		sponsorsContainer.classList.remove("animation");
