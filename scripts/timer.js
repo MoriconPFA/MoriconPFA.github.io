dbef7935d0156fda792922b8fe3d151cc38034e8
Tylyps <jakubb.chmielewskii@gmail.com>
2023-08-12 17:56:40 +0200

    Fix timer for firefox

--- scripts/timer.js
+++ scripts/timer.js
@@ -1,6 +1,7 @@
 const timeObject = document.querySelector(".timeRemining");
-const moriconTime = new Date("09-08-2023 14:00");
-// const moriconTime = new Date("08-08-2023 14:00");
+// const moriconTime = new Date("09-08-2023T14:00");
+const moriconTime = new Date("2023-09-08T14:00");
+// const moriconTime = new Date("08-08-2022 14:00");
 let timer = null;
 
 const renderTime = () => {
@@ -10,6 +11,9 @@ const renderTime = () => {
 	let days = 0;
 	const now = new Date();
 	let deltaTime = moriconTime.getTime() - now.getTime();
+	console.log("now", now, now.getTime())
+	console.log("moriconTime", moriconTime, moriconTime.getTime())
+	console.log(deltaTime)
 
 	if (deltaTime > 0) {
 		sec = Math.floor((deltaTime / 1000) % 60);
@@ -30,6 +34,6 @@ const renderTime = () => {
 
 }
 
-
+new Date('1995-12-17T03:24:00')
 renderTime();
 timer = setInterval(renderTime, 1000);
