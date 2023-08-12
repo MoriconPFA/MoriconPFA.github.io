cdc9f1a66c3a496e41c8202a70afe64f85f539e8
Tylyps <jakubb.chmielewskii@gmail.com>
2023-08-12 18:06:24 +0200

    Clear console logs and add end time message

--- scripts/timer.js
+++ scripts/timer.js
@@ -1,7 +1,7 @@
 const timeObject = document.querySelector(".timeRemining");
-// const moriconTime = new Date("09-08-2023T14:00");
-const moriconTime = new Date("2023-09-08T14:00");
-// const moriconTime = new Date("08-08-2022 14:00");
+// const moriconStartTime = new Date("09-08-2023T14:00");
+const moriconStartTime = new Date("2023-09-08 14:00");
+const moriconEndTime = new Date("2023-09-11 14:00");
 let timer = null;
 
 const renderTime = () => {
@@ -10,24 +10,26 @@ const renderTime = () => {
 	let hours = 0;
 	let days = 0;
 	const now = new Date();
-	let deltaTime = moriconTime.getTime() - now.getTime();
-	console.log("now", now, now.getTime())
-	console.log("moriconTime", moriconTime, moriconTime.getTime())
-	console.log(deltaTime)
+	const deltaStartTime = moriconStartTime.getTime() - now.getTime();
+	const deltaEndTime = moriconEndTime.getTime() - now.getTime();
 
-	if (deltaTime > 0) {
-		sec = Math.floor((deltaTime / 1000) % 60);
-		min = Math.floor((deltaTime / (1000 * 60)) % 60);
-		hours = Math.floor((deltaTime / (1000 * 60 * 60)) % 24);
-		days = Math.floor((deltaTime / (1000 * 60 * 60 * 24)));
+	if (deltaStartTime > 0) {
+		sec = Math.floor((deltaStartTime / 1000) % 60);
+		min = Math.floor((deltaStartTime / (1000 * 60)) % 60);
+		hours = Math.floor((deltaStartTime / (1000 * 60 * 60)) % 24);
+		days = Math.floor((deltaStartTime / (1000 * 60 * 60 * 24)));
 
 		sec = sec < 10 ? `0${sec}` : `${sec}`;
 		min = min < 10 ? `0${min}` : `${min}`;
 		hours = hours < 10 ? `0${hours}` : `${hours}`;
 
 		timeObject.innerHTML = `${!!days ? `${days} dni ` : ""} ${hours}:${min}:${sec}`
-	} else {
+	} else if (deltaEndTime > 0) {
 		timeObject.innerHTML = "Widzimy się na miejscu"
+		document.querySelector("#odliczanie h2:first-child:not(h2.timeRemining)")?.remove();
+		clearInterval(timer);
+	} else {
+		timeObject.innerHTML = "Widzimy się za rok 😄"
 		document.querySelector("#odliczanie h2:first-child:not(h2.timeRemining)")?.remove();
 		clearInterval(timer);
 	}
